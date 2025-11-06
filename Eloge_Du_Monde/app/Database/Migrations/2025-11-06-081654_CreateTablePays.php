458a01833839ca99c28d884c185e36f7b3cecf4c
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-11-06 09:19:16 +0100

    migration table pays

---
+++ Eloge_Du_Monde/app/Database/Migrations/2025-11-06-081654_CreateTablePays.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTablePays extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'SERIAL',
+				'unsigned'       => true,
+				'auto_increment' => true,
+			],
+			'nom' => [
+				'type'=> 'VARCHAR',
+				'constraint'=> 100,
+				'nullable'=> false,
+			],
+			'continent' => [
+				'type'=> 'VARCHAR',
+				'constraint'=> 50,
+				'nullable'=> false,
+			],
+			'cout' => [
+				'type'=> 'INT',
+				'nullable'=> false,
+			],
+		]);
+
+		$this->forge->addKey('id', true);
+		$this->forge->createTable('pays');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('pays');
+	}
+}
