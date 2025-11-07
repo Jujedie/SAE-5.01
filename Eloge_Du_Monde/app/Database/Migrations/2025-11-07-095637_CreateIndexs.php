b07b0c4f440be86aa1df810d1d954df73dae0c95
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-11-07 11:02:57 +0100

    migration indexs

---
+++ Eloge_Du_Monde/app/Database/Migrations/2025-11-07-095637_CreateIndexs.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateIndexs extends Migration
+{
+	public function up()
+	{
+		$this->db->query("
+		CREATE INDEX idx_destination_pays      ON DESTINATION(id_pays);
+		CREATE INDEX idx_journaux_utilisateur  ON JOURNAUX   (id_utilisateur);
+		CREATE INDEX idx_posteblog_utilisateur ON POSTEBLOG  (id_utilisateur);
+		CREATE INDEX idx_avis_utilisateur      ON AVIS       (id_utilisateur);
+		CREATE INDEX idx_voyage_utilisateur    ON VOYAGE     (id_utilisateur);
+		CREATE INDEX idx_heberger_destination  ON HEBERGER   (id_destination);");
+	}
+
+	public function down()
+	{
+		$this->db->query("
+		DROP INDEX IF EXISTS idx_destination_pays;
+		DROP INDEX IF EXISTS idx_journaux_utilisateur;
+		DROP INDEX IF EXISTS idx_posteblog_utilisateur;
+		DROP INDEX IF EXISTS idx_avis_utilisateur;
+		DROP INDEX IF EXISTS idx_voyage_utilisateur;
+		DROP INDEX IF EXISTS idx_heberger_destination;");
+	}
+}
