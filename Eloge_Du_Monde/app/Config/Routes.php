ccd1e8bb93a89a1801a171b58f713b955bf0e676
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-12-02 09:45:49 +0100

    delete email & database

--- Eloge_Du_Monde/app/Config/Routes.php
+++ Eloge_Du_Monde/app/Config/Routes.php
@@ -3,6 +3,28 @@
 use CodeIgniter\Router\RouteCollection;
 
 /**
- * @var RouteCollection $routes
+ * @var RouteCollection
  */
-$routes->get('/', 'Home::index');
+
+// Route par defaut
+$routes->get('/', 'ConnexionController::index');
+
+// Connexion
+$routes->get('connexion'      , 'ConnexionController::index'      );
+$routes->get('deconnexion'    , 'ConnexionController::deconnexion');
+$routes->match(['GET', 'POST'], 'ConnexionController/connexion', 'ConnexionController::connexion');
+
+// Inscription
+$routes->get('inscription'    , 'InscriptionController::index');
+$routes->match(['GET', 'POST'], 'InscriptionController/enregistrer', 'InscriptionController::enregistrer');
+
+// Mot de passe oublie
+$routes->get('oublieMdp'      , 'OublieMdpController::index');
+$routes->match(['GET', 'POST'], 'OublieMdpController/envoyerLienReinitialisation', 'OublieMdpController::envoyerLienReinitialisation');
+
+// Reinitialisation mot de passe
+$routes->get('reinitialiserMdp/(:segment)', 'ReinitialiserMdpController::index/$1');
+$routes->match(['GET', 'POST']            , 'ReinitialiserMdpController/majMdp', 'ReinitialiserMdpController::majMdp');
+
+// Accueil
+$routes->get('accueil'  , 'AccueilController::index' , ['filter' => 'authGuard']);
