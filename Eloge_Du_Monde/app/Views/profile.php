812c240f1175ec35bf5bae62ac16fba5d8e70780
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-03 14:15:53 +0100

    Suppression de la vue profil.php et création de profile.php avec la structure de base.

---
+++ Eloge_Du_Monde/app/Views/profile.php
@@ -0,0 +1,17 @@
+<?= $this->extend('layout/default') ?>
+
+<?= $this->section('title') ?>Profil<?= $this->endSection() ?>
+
+<?= $this->section('styles') ?>
+<link rel="stylesheet" href="<?= base_url('assets/css/profil.css') ?>">
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+ 
+
+<?= $this->endSection() ?>
+
+<?= $this->section('scripts') ?>
+<script src="<?= base_url('assets/js/profil.js') ?>"></script>
+<?= $this->endSection() ?>
