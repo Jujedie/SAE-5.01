6aeaffe4d5d6aca5e5d7d64c5f914a377c5ccfed
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-04 14:08:20 +0100

    Ajout de la méthode index dans TripController pour gérer les filtres des voyages

--- Eloge_Du_Monde/app/Controllers/TripController.php
+++ Eloge_Du_Monde/app/Controllers/TripController.php
@@ -6,5 +6,18 @@ use App\Models\UserModel;
 
 class TripController extends BaseController
 {
-	public function 
+	public function index($filter = []){
+		$session = session();
+
+		$trips = [];
+		if (!empty($filter)) {
+			$tripModel = new \App\Models\TripModel();
+			$trips = $tripModel->getTripsByFilter($filter);
+		} else {
+			$tripModel = new \App\Models\TripModel();
+			$trips = $tripModel->getAllTrips();
+		}
+
+		return view('trips', ["listTrips" => $trips]);
+	}
 }
