2abee99bc9d3bab57add89bddf514d4c45ddd480
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-04 14:25:09 +0100

    Refactor de la méthode index dans TripController pour améliorer la lisibilité et l'initialisation du modèle TripModel

--- Eloge_Du_Monde/app/Controllers/TripController.php
+++ Eloge_Du_Monde/app/Controllers/TripController.php
@@ -2,22 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\TripModel;
 use App\Models\UserModel;
 
 class TripController extends BaseController
 {
-	public function index($filter = []){
+	public function index($filter = [])
+	{
 		$session = session();
+		$tripModel = new TripModel();
 
 		$trips = [];
-		if (!empty($filter)) {
-			$tripModel = new \App\Models\TripModel();
+		if (!empty($filter)) 
+		{
 			$trips = $tripModel->getTripsByFilter($filter);
-		} else {
-			$tripModel = new \App\Models\TripModel();
+		} 
+		else 
+		{
 			$trips = $tripModel->getAllTrips();
 		}
 
-		return view('trips', ["listTrips" => $trips]);
+		return view('trips', ["user" => ((new UserModel())->getUserById($session->get('idUser'))), "listTrips" => $trips]);
 	}
 }
