da32cd8dbebf51f13737a240426e5d4b7e999ef7
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-12-09 09:37:34 +0100

    remove bootstrap link

--- Eloge_Du_Monde/app/Views/layouts/default.php
+++ Eloge_Du_Monde/app/Views/layouts/default.php
@@ -214,7 +214,6 @@
 	</div>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= base_url('assets/js/notifications.js') ?>"></script>
 <script src="<?= base_url('assets/js/home.js') ?>"></script>
 <?= $this->renderSection('scripts') ?>
