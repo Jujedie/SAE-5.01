f1f0756e7e815de7b25d3e36d8ca12a3062590f1
Julien Oyer <oyer.ludovic@gmail.com>
2025-12-09 09:51:07 +0100

    ajout de la vue de contact avec les sections de styles, contenu et scripts

---
+++ Eloge_Du_Monde/app/Views/contact.php
@@ -0,0 +1,17 @@
+<?= $this->extend('layouts/default') ?>
+
+<?= $this->section('styles') ?>
+
+<?= $this->endSection() ?>
+
+
+<?= $this->section('content') ?>
+
+
+
+<?= $this->endSection() ?>
+
+
+<?= $this->section('scripts') ?>
+
+<?= $this->endSection() ?>
