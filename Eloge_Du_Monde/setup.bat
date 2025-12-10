593f784f80c958fd5f7284c2c98d9fdfe9f4ab74
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-12-10 11:14:05 +0100

    script setup sh et bat

--- Eloge_Du_Monde/setup.bat
+++ Eloge_Du_Monde/setup.bat
@@ -1,10 +1,11 @@
-#!/bin/bash
+@echo off
 
+php spark migrate:rollback --all
 php spark migrate
 php spark db:seed UserSeeder
 php spark db:seed CountrySeeder
 php spark db:seed BlogPostSeeder
 php spark db:seed ReviewSeeder
 php spark db:seed TripStepSeeder
-php spark serve
-xdg-open http://localhost:8080 &
+start php spark serve
+start http://localhost:8080
