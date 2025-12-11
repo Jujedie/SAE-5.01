eddce77f0896dfda5cc199d6b4cf1e655ae6b48b
Thomazeau-Agullo Louis <louisagullo.05@gmail.com>
2025-12-11 10:07:58 +0100

    indent migrations

--- Eloge_Du_Monde/app/Database/Migrations/2025-11-07-110000_CreateTableHost.php
+++ Eloge_Du_Monde/app/Database/Migrations/2025-11-07-110000_CreateTableHost.php
@@ -6,41 +6,39 @@ use CodeIgniter\Database\Migration;
 
 class CreateTableHost extends Migration
 {
-    public function up()
-    {
-        $this->forge->addField
-        ([
-            'idTrip'         =>
-            [
-                'type'       => 'INT',
-                'null'       => false,
-            ],
-            'idTripStep'     =>
-            [
-                'type'       => 'INT',
-                'null'       => false,
-            ],
-            'nbDays'     =>
-            [
-                'type'   => 'INT',
-                'null'   => false,
-            ],
-            'nbNights'   =>
-            [
-                'type'   => 'INT',
-                'null'   => false,
-            ],
-        ]);
+	public function up()
+	{
+		$this->forge->addField
+		([
+			'idTrip'         =>
+			[
+				'type'       => 'INT',
+				'null'       => false,
+			],
+			'idTripStep'     =>
+			[
+				'type'       => 'INT',
+				'null'       => false,
+			],
+			'nbDays'     =>
+			[
+				'type'   => 'INT',
+				'null'   => false,
+			],
+			'nbNights'   =>
+			[
+				'type'   => 'INT',
+				'null'   => false,
+			],
+		]);
 
-        $this->forge->addKey(['idTrip', 'idTripStep'], true);
-        // Note: No FK on idTrip because PostgreSQL table inheritance (INHERITS) 
-        // doesn't work well with foreign keys - the parent table doesn't see child rows
-        $this->forge->addForeignKey('idTripStep', 'tripStep', 'idTripStep', 'CASCADE', 'CASCADE');
-        $this->forge->createTable('host', true);
-    }
+		$this->forge->addKey(['idTrip', 'idTripStep'], true);
+		$this->forge->addForeignKey('idTripStep', 'tripStep', 'idTripStep', 'CASCADE', 'CASCADE');
+		$this->forge->createTable('host', true);
+	}
 
-    public function down()
-    {
-        $this->forge->dropTable('host', true, true);
-    }
+	public function down()
+	{
+		$this->forge->dropTable('host', true, true);
+	}
 }
